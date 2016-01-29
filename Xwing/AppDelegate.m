52c0de764223ce766f99b570fe9e948717da930c
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-01-29 18:34:38 +0100

    Modifico Transformada

--- Xwing/AppDelegate.m
+++ Xwing/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "KCGXWingViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    KCGXWingViewController *wingVC = [[KCGXWingViewController alloc] init];
+    
+    self.window.rootViewController = wingVC;
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
--- Xwing/KCGXWingViewController.m
+++ Xwing/KCGXWingViewController.m
@@ -9,6 +9,8 @@
 #import "KCGXWingViewController.h"
 
 @interface KCGXWingViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *xwingView;
+@property (weak, nonatomic) IBOutlet UIImageView *spaceView;
 
 @end
 
@@ -17,6 +19,129 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    //Creamos el reconocedor de Taps
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(tap:)];
+    
+    //Creo el reconocedor de SWIPE IZQUIERDA
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc]
+                                       initWithTarget:self
+                                       action:@selector(swipe:)];
+    
+    //Creo el reconocedor de SWIPE DERECHA
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc]
+                                           initWithTarget:self
+                                           action:@selector(swipe:)];
+    
+    //Lo encasquetamos a self.spaceView
+    [self.spaceView addGestureRecognizer:tap];
+
+    [self.spaceView addGestureRecognizer:swipeLeft];
+    
+    [self.spaceView addGestureRecognizer:swipeRight];
+    
+}
+
+//Animaciones al tocar pantalla
+-(void) tap: (UITapGestureRecognizer *) recognizer {
+    
+    if (recognizer.state == UIGestureRecognizerStateRecognized) {
+        
+        UIViewAnimationOptions options =
+            UIViewAnimationOptionCurveEaseInOut |
+            UIViewAnimationOptionBeginFromCurrentState;
+        
+        //Translación
+        [UIView animateWithDuration:1.0
+                              delay:0.0
+                            options:options
+                         animations:^{
+            
+                             self.xwingView.center =
+                                [recognizer locationInView:
+                                 self.spaceView];
+            
+                         }completion:^(BOOL finished) {
+            
+                         }];
+        
+        //Rotación
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:options
+                         animations:^{
+            
+                             self.xwingView.transform = CGAffineTransformMakeRotation(M_PI_2);
+        
+        } completion:^(BOOL finished) {
+            
+            [UIView animateWithDuration:0.5
+                                  delay:0.0
+                                options:options
+                             animations:^{
+                                 
+                                 self.xwingView.transform = CGAffineTransformIdentity;
+                             
+                             } completion:^(BOOL finished) {
+                                 
+                                    //NADA
+                                 
+                                 }];
+            
+        }];
+        
+    }
+    
+}
+
+-(void) swipe: (UISwipeGestureRecognizer *) recognizer {
+    
+    if (recognizer.direction == UISwipeGestureRecognizerDirectionLeft) {
+        
+        UIViewAnimationOptions options =
+        UIViewAnimationOptionCurveEaseInOut |
+        UIViewAnimationOptionBeginFromCurrentState;
+    
+        //SWIPE
+        [UIView animateWithDuration:0.9
+                              delay:0.0
+                            options:options
+                         animations:^{
+            
+                             self.xwingView.transform = CGAffineTransformMakeRotation(M_2_PI);
+        
+                         } completion:^(BOOL finished) {
+                             
+                             //NADA
+                         
+                         }];
+        
+    }
+    
+    if (recognizer.direction == UISwipeGestureRecognizerDirectionRight) {
+        
+        UIViewAnimationOptions options =
+        UIViewAnimationOptionCurveEaseInOut |
+        UIViewAnimationOptionBeginFromCurrentState;
+        
+        //SWIPE
+        [UIView animateWithDuration:0.9
+                              delay:0.0
+                            options:options
+                         animations:^{
+                             
+                             self.xwingView.transform = CGAffineTransformMakeRotation(M_PI);
+                             
+                         } completion:^(BOOL finished) {
+                             
+                             //NADA
+                             
+                         }];
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
