65b7b746be613187e998f913eff7275d438a3eff
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-01-29 18:24:30 +0100

    Meto Swipe izquierda & derecha

--- Xwing/KCGXWingViewController.h
+++ Xwing/KCGXWingViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  KCGXWingViewController.h
 //  Xwing
 //
 //  Created by Adrian Perez Lopez on 25/1/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface KCGXWingViewController : UIViewController
 
 @end
-
---
+++ Xwing/KCGXWingViewController.m
@@ -0,0 +1,37 @@
+//
+//  KCGXWingViewController.m
+//  Xwing
+//
+//  Created by Adrian Perez Lopez on 25/1/16.
+//  Copyright © 2016 Adrian Perez Lopez. All rights reserved.
+//
+
+#import "KCGXWingViewController.h"
+
+@interface KCGXWingViewController ()
+
+@end
+
+@implementation KCGXWingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
